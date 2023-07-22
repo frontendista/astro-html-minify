995c94b7e419aac7e2dc2a337a3759bdcaeaa00d
Pavel Sušický <me@frontendista.cz>
2023-07-22 14:43:40 +0200

    chore(build): replace parcel with tsup

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
-import minify from "../src";
+// import minify from "../src";
+import minify from "@frontendista/astro-html-minify";
 
 export default defineConfig({
+	compressHTML: true,
+	output: "hybrid",
 	integrations: [minify()]
 });
--- src/optimizers/css.ts
+++ src/optimizers/css.ts
@@ -1,26 +1,27 @@
-import { transform, transformStyleAttribute } from "@parcel/css";
+import { transform, transformStyleAttribute } from "lightningcss";
 
 /**
- * Transforms and minifies CSS using @parcel/css package.
+ * Transforms and minifies CSS using lightningcss package.
  *
  * @param originalCode Code to be transformed and minified.
  * @param type Whether the CSS comes from inline attribute or CSS stylesheet.
+ *
  * @returns transformed and minified code.
  */
 export function minifyCSS(originalCode: string, type: "inline" | undefined) {
-  const code = Buffer.from(originalCode, "utf-8");
+	const code = Buffer.from(originalCode, "utf-8");
 
-  const { code: transformedHTML } =
-    type === "inline"
-      ? transformStyleAttribute({
-          code,
-          minify: true,
-        })
-      : transform({
-          code,
-          filename: "style-tag.css",
-          minify: true,
-        });
+	const { code: transformedHTML } =
+		type === "inline"
+			? transformStyleAttribute({
+					code,
+					minify: true
+			  })
+			: transform({
+					code,
+					filename: "style-tag.css",
+					minify: true
+			  });
 
-  return transformedHTML.toString("utf-8");
+	return transformedHTML.toString("utf-8");
 }
