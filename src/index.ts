c93a2ec9fbf214202873b9e9c22df139a50e977a
Pavel Sušický <me@frontendista.cz>
2023-07-24 12:11:46 +0200

    feat(major): revamp library internals and API

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,62 @@
-import merge from "deepmerge";
+import { readFile, writeFile } from "node:fs/promises";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
 
-import { minify } from "./minify";
-import { defaultOptions, type IntegrationOptions } from "./options";
+import { glob } from "glob";
+import {
+	Minifier,
+	type TerserOptions,
+	type LightningCSSOptions,
+	type ESBuildOptions
+} from "@frontendista/html-minifier-terser";
 
 import type { AstroIntegration } from "astro";
 
-export default function createIntegration(
-  options: IntegrationOptions = {}
-): AstroIntegration {
-  options = merge(defaultOptions, options);
+interface IntegrationOptions {
+	html?: TerserOptions;
+	css?: LightningCSSOptions;
+	js?: ESBuildOptions;
+}
 
-  return {
-    name: "@frontendista/astro-minify",
-    hooks: {
-      "astro:config:setup": ({ config }) => {
-        if (typeof options.reportCompressedSize === "undefined") {
-          options.reportCompressedSize = config.vite.build
-            ? config.vite.build.reportCompressedSize
-            : true;
-        }
-      },
-      "astro:build:done": async ({ dir }) => {
-        await minify(dir.href, options);
-      },
-    },
-  };
+export default function createIntegration(options?: IntegrationOptions): AstroIntegration {
+	const minifier = new Minifier()
+		.withHTMLOptions(options?.html || {})
+		.withCSSOptions(options?.css || {})
+		.withJSOptions(options?.js || {});
+
+	return {
+		name: "@frontendista/astro-minify",
+		hooks: {
+			"astro:build:done": async ({ dir }) => {
+				const cwd = fileURLToPath(dir);
+
+				const htmlFiles = await glob("**/*.html", {
+					cwd
+				});
+
+				if (htmlFiles.length === 0) {
+					return;
+				}
+
+				for (const filename of htmlFiles) {
+					const filePath = join(cwd, filename);
+
+					let html = await readFile(filePath, "utf8");
+
+					// html = removeTestAttributes
+					// 	? [/data-test=".*?"/g, /data-test-id=".*?"/g, /data-testid=".*?"/g].reduce(
+					// 			(currentHTML, attributeRegex) => {
+					// 				return currentHTML.replace(attributeRegex, "");
+					// 			},
+					// 			html
+					// 	  )
+					// 	: html;
+
+					const optimizedHTML = await minifier.minify(html);
+
+					await writeFile(filePath, optimizedHTML, "utf-8");
+				}
+			}
+		}
+	};
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,35 +1,35 @@
-import { cyan, green, dim } from "kleur/colors";
-import { gzip } from "node:zlib";
-import { promisify } from "node:util";
+// import { cyan, green, dim } from "kleur/colors";
+// import { gzip } from "node:zlib";
+// import { promisify } from "node:util";
 
-export async function reportSize(
-  filename: string,
-  original: string,
-  transformed: string,
-  logCompressed?: boolean
-) {
-  const compress = promisify(gzip);
-  const beforeSize = (Buffer.byteLength(original) / 1024).toFixed(2);
-  const afterSize = (Buffer.byteLength(transformed) / 1024).toFixed(2);
+// export async function reportSize(
+//   filename: string,
+//   original: string,
+//   transformed: string,
+//   logCompressed?: boolean
+// ) {
+//   const compress = promisify(gzip);
+//   const beforeSize = (Buffer.byteLength(original) / 1024).toFixed(2);
+//   const afterSize = (Buffer.byteLength(transformed) / 1024).toFixed(2);
 
-  process.stdout.write(
-    `${cyan(filename)} optimized from ${beforeSize} KiB to ${green(
-      `${afterSize} KiB`
-    )}`
-  );
+//   process.stdout.write(
+//     `${cyan(filename)} optimized from ${beforeSize} KiB to ${green(
+//       `${afterSize} KiB`
+//     )}`
+//   );
 
-  if (logCompressed) {
-    const { byteLength } = await compress(transformed);
-    const compressedSize = (byteLength / 1024).toFixed(2);
-    process.stdout.write(dim(` / gzip: ${compressedSize} KiB`));
-  }
+//   if (logCompressed) {
+//     const { byteLength } = await compress(transformed);
+//     const compressedSize = (byteLength / 1024).toFixed(2);
+//     process.stdout.write(dim(` / gzip: ${compressedSize} KiB`));
+//   }
 
-  process.stdout.write("\n");
-}
+//   process.stdout.write("\n");
+// }
 
-export function getTimeStat(timeStart: number, timeEnd: number) {
-  const buildTime = timeEnd - timeStart;
-  return buildTime < 750
-    ? `${Math.round(buildTime)}ms`
-    : `${(buildTime / 1000).toFixed(2)}s`;
-}
+// export function getTimeStat(timeStart: number, timeEnd: number) {
+//   const buildTime = timeEnd - timeStart;
+//   return buildTime < 750
+//     ? `${Math.round(buildTime)}ms`
+//     : `${(buildTime / 1000).toFixed(2)}s`;
+// }
